0cb0ab88c73634fa29e52ed0bd91f87ba683c11e
mubashra-danish <mubashradanish26@gmail.com>
2026-04-29 18:54:56 +0530

    added theme

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
 
       <Routes>
        <Route path="/" element={<Home />} />
-<Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/login" element={<Login/>} />
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import { useContext,useEffect} from 'react'
+import { ThemeContext } from '../context/ThemeContext'
 const Login = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext)
+  const handleLogin = (e) =>{
+    e.preventDefault()
+    console.log("loginn")
+  }
+ useEffect(() => {
+    document.body.className = theme
+  }, [theme])
   return (
-    <div>Login</div>
+    <div className="container">
+      <form onSubmit={handleLogin} className='form'>
+        <h2>Login</h2>
+
+        <input type="email" placeholder="Enter Email" required />
+        <input type="password" placeholder="Enter Password" required />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
 
